72ad63eababd1759f7d37ade5307740903b2a296
Robin kron <krobin.it@gmail.com>
2026-06-25 20:11:34 +0300

    ncurses added and tested

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <ncurses.h>
 
 typedef struct Task {
   char text[100];
@@ -7,14 +7,29 @@ typedef struct Task {
 
 int main()
 {
+  int scr_rows, scr_cols;
+  int task_box_width = 50;
+  int task_box_height = 20;
 
   Task task = {
     .text = "Create first task",
     .completed = true
   };
 
-  printf("Task: %s\n", task.text);
-  printf("Done: %s", task.completed ? "yes" : "no");
+  initscr();
+  getmaxyx(stdscr, scr_rows, scr_cols);
 
+  WINDOW *win = newwin(task_box_height, task_box_width, (scr_rows - task_box_height) /2, (scr_cols - task_box_width) /2);
+  box(win, 0,0);
+
+
+  mvwprintw(win, 1,2, "* %s [%s]", task.text, task.completed ? "x" : " ");
+  
+  while (1) {
+    wrefresh(win);
+    napms(100);
+  }
+  
+  endwin();
   return 0;
 }
